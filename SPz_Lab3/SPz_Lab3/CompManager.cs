6183ab321e1bd48606ffe9665ca8edb54707a275
Dima Moruha <trluxsus@gmail.com>
2019-10-18 23:01:25 +0300

    SPz_Lab3 class CompManager
    Implemnt fields and prop's
    
    Signed-off-by: Dima Moruha <trluxsus@gmail.com>

---
+++ SPz_Lab3/SPz_Lab3/CompManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPz_Lab3
+{
+    class CompManager
+    {
+
+        //CompManager fields & properies
+        private List<Computer> CMComputersList { get; set; }
+        private Dictionary<Task,int> CMTaskDictionary { get; set; }
+        private int CMComputersAmount { get; set; }
+        private int CMWorkplaceAmount { get; set; }
+        private bool CMWiFiStatus { get; set; }
+        private string CMWiFiPswd { get; set; }
+    }
+}
