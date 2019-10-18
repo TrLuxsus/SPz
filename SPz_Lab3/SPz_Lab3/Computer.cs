d74f0765c1a4a9cdcb67775ddc2ab1f78a790679
Dima Moruha <trluxsus@gmail.com>
2019-10-18 21:28:14 +0300

    SPz_Lab3 class Computer fields
    Implement fields of class Computer
    
    Signed-off-by: Dima Moruha <trluxsus@gmail.com>

---
+++ SPz_Lab3/SPz_Lab3/Computer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPz_Lab3
+{
+    class Computer
+    {
+        //Computer fields
+
+        private string CName;
+
+        private List<Task> CAssignedTasks;
+
+        private int CAmountOfAssgndTsks;
+
+        private bool CStatus;
+
+    }
+}
