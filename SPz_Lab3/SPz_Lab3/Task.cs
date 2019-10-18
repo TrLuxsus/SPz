cb0483d2fd4a1b56f495266dfe56db87dcfd897f
Dima Moruha <trluxsus@gmail.com>
2019-10-18 16:40:15 +0300

    SPz_Lab3 class Task fields
    Implement fields of class Task
    
    Signed-off-by: Dima Moruha <trluxsus@gmail.com>

---
+++ SPz_Lab3/SPz_Lab3/Task.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SPz_Lab3
+{
+    class Task
+    {
+        private string TaskName;
+        private bool TaskCompleteStatus;
+        private DateTime TaskDateTime;
+        private string TaskAdditionalInf;
+    }
+}
